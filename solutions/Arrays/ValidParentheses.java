4f410fb7ed3f9c8e0cd824aac729fa87919dbe0d
Rohan Bhalerao <85570335+Rohanbhalerao875@users.noreply.github.com>
2026-07-06 22:28:46 +0530

    Add isValid method for ValidParentheses solution
    Implement isValid method to check valid parentheses.

--- solutions/Arrays/ValidParentheses.java
+++ solutions/Arrays/ValidParentheses.java
@@ -7,7 +7,34 @@
  */
 
 class Solution {
-    public void solve() {
-        // TODO: implement ValidParentheses
+    public boolean isValid(String s) {
+        Stack <Character> st = new Stack <>();
+        char [] word = s.toCharArray();
+        if(s.length()==0 || s.length()==1){
+            return false;
+        }
+
+        for(int i=0;i<s.length();i++){
+           if(word[i]=='{'){
+            st.push('}');
+           } 
+          if(word[i]=='['){
+            st.push(']');
+          }
+           if(word[i]=='('){
+            st.push(')');
+           }
+           if(st.size()!=0 && st.peek()==word[i]){
+                st.pop();
+           }
+               
+        }
+        
+        if(st.size()==0){
+            return true;
+        }else{return false;}
+
+
+
     }
 }
