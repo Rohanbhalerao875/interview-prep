f9653f7a9f9d398cc42d3cbae339364bdf2cf7e8
Rohan Bhalerao <85570335+Rohanbhalerao875@users.noreply.github.com>
2026-07-07 22:38:55 +0530

    Add myAtoi method for string to integer conversion
    Implemented the myAtoi method to convert a string to an integer, handling leading spaces, signs, and overflow cases.

--- solutions/Arrays/StringToIntegerAtoi.java
+++ solutions/Arrays/StringToIntegerAtoi.java
@@ -7,7 +7,45 @@
  */
 
 class Solution {
-    public void solve() {
-        // TODO: implement StringToIntegerAtoi
+    public int myAtoi(String s) {
+        int n = s.length();
+        int i=0;
+        while(i<n && s.charAt(i)==' '){
+            i++;
+        }        
+        if (i >= n) return 0;
+        int result = 0;
+        int sign = 1;
+       
+        if(i<n){
+             char ch = s.charAt(i);
+            if(ch=='-'){
+                sign=sign*-1;
+                i++;
+            }
+            else if(ch=='+'){
+                i++;
+            }
+        }
+        int MaxValue = Integer.MAX_VALUE;
+        int maxDiv10 = MaxValue / 10;
+        int maxLast = MaxValue % 10;
+        while(i<n){
+            char ch = s.charAt(i);
+            
+            
+            if(ch<'0' || ch>'9'){
+                break;
+            }
+            
+            int digit = s.charAt(i)-'0';
+
+            if (result > maxDiv10 || (result == maxDiv10 && digit > maxLast)){   // (result > maxDiv10 || (result == maxDiv10 && digit > maxLastDigit)) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            result = result*10 + digit;
+            i++;
     }
+                return sign*result;
+}
 }
