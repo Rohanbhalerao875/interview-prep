eeaa2fe3ea6c9f96424f6382393a3c0d5efbba71
Rohan Bhalerao <85570335+Rohanbhalerao875@users.noreply.github.com>
2026-07-08 11:48:33 +0530

    Implement mergeTwoLists method for sorted lists
    Implemented the mergeTwoLists method to merge two sorted linked lists into one sorted linked list.

--- solutions/LinkedLists/MergeTwoSortedLists.java
+++ solutions/LinkedLists/MergeTwoSortedLists.java
@@ -6,8 +6,74 @@
  * TODO: implement solution for MergeTwoSortedLists
  */
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-    public void solve() {
-        // TODO: implement MergeTwoSortedLists
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode ans = new ListNode(-1);
+        ListNode temp1 = list1;
+        ListNode temp2 = list2;
+        ListNode tail = ans;
+
+        if(list1== null){
+            return list2;
+        }
+        if(list2== null){
+            return list1;
+        }
+        if(list1== null && list2==null){
+            return ans.next;
+        }
+        
+        if(temp1.val<=temp2.val){
+            tail.next = temp1;
+            temp1 = temp1.next;
+            tail = tail.next;
+        }else{
+            tail.next = temp2;
+            temp2=temp2.next;
+            tail = tail.next;
+        }
+
+        while(temp1!=null && temp2!=null){
+        if(temp1.val<=temp2.val){
+            tail.next = temp1;
+                temp1 = temp1.next;
+                tail = tail.next;
+            
+        }else{
+            tail.next = temp2;
+
+                temp2 = temp2.next;
+            tail = tail.next;
+            
+        }
+        }
+        while(temp1!=null){
+            tail.next = temp1;
+
+                temp1 = temp1.next;
+            tail = tail.next;
+            
+        }
+        while(temp2!=null){
+                tail.next = temp2;
+   
+                temp2 = temp2.next;
+            tail = tail.next;
+            
+        }
+
+        
+    
+    return ans.next;
     }
 }
