d63253332fa4ad25c8fcc719f52fe4d4e1d50b58
Rohan Bhalerao <85570335+Rohanbhalerao875@users.noreply.github.com>
2026-07-20 21:21:10 +0530

    Implement reorderList method in Solution class phase 1 one more solution incomming
    Implemented the reorderList method to reorder a singly-linked list based on the specified pattern.

--- solutions/LinkedLists/ReorderList.java
+++ solutions/LinkedLists/ReorderList.java
@@ -6,8 +6,67 @@
  * TODO: implement solution for ReorderList
  */
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-    public void solve() {
-        // TODO: implement ReorderList
+    public void reorderList(ListNode head) {
+        HashMap<Integer,ListNode> hm = new HashMap<>();
+
+        ListNode temp = head;
+        int size = 0;
+
+        while(temp!=null){
+            temp=temp.next;
+            size++;
+        }
+        
+        if(size == 1){
+            return;
+        }
+        if(size == 0){
+            return;
+        }
+        temp = head;
+        int count = 1;
+        while(temp!=null){
+            hm.put(count,temp);
+            temp = temp.next;
+            count+=1;
+        }
+        temp = head;
+
+        int left =2;
+        int right = size;
+        int counter=0;
+        while(left<=right){
+            
+                temp.next = hm.get(right);
+                right--;
+                temp=temp.next;
+                
+                if(left>right){
+                    break;
+                }
+            
+                temp.next = hm.get(left);
+                temp =temp.next;
+                System.out.print(temp.val);
+                left++;
+            
+            
+        }
+        temp.next = null;
+        
+
+
+        
     }
 }
