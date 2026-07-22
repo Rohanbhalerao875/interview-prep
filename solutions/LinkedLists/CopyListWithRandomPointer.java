fc01d14b824c891cd48584235cf4328547e76609
Rohan Bhalerao <85570335+Rohanbhalerao875@users.noreply.github.com>
2026-07-22 23:23:32 +0530

    Add implementation for CopyListWithRandomPointer
    Implemented the copyRandomList method to create a deep copy of a linked list with random pointers.

--- solutions/LinkedLists/CopyListWithRandomPointer.java
+++ solutions/LinkedLists/CopyListWithRandomPointer.java
@@ -1,13 +1,42 @@
-﻿/**
- * Problem: CopyListWithRandomPointer
- * Difficulty: TBD
- * URL: https://leetcode.com/problems/
- *
- * TODO: implement solution for CopyListWithRandomPointer
- */
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
 
 class Solution {
-    public void solve() {
-        // TODO: implement CopyListWithRandomPointer
+    public Node copyRandomList(Node head) {
+        HashMap<Node,Node> hm = new HashMap<>();
+        
+        Node temp = head;
+        
+        while (temp != null){
+            hm.put(temp,new Node(temp.val));
+            temp=temp.next;
+        }
+        temp = head;
+
+        while(temp!=null){
+            hm.get(temp).next = hm.get(temp.next);
+            hm.get(temp).random = hm.get(temp.random);
+            temp = temp.next;
+
+        }
+        return hm.get(head);
+
+// map.get(temp).next = map.get(temp.next);
+// 'B'.next = C'
+
+
+
     }
 }
