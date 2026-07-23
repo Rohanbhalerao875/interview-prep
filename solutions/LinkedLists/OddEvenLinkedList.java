781def093e206004ef77f98cea7f7023ac19b45e
Rohan Bhalerao <85570335+Rohanbhalerao875@users.noreply.github.com>
2026-07-24 00:31:32 +0530

    Add solution for OddEvenLinkedList problem
    Implemented the oddEvenList method to rearrange the linked list.

--- solutions/LinkedLists/OddEvenLinkedList.java
+++ solutions/LinkedLists/OddEvenLinkedList.java
@@ -1,13 +1,34 @@
-﻿/**
- * Problem: OddEvenLinkedList
- * Difficulty: TBD
- * URL: https://leetcode.com/problems/
- *
- * TODO: implement solution for OddEvenLinkedList
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
  */
-
 class Solution {
-    public void solve() {
-        // TODO: implement OddEvenLinkedList
+    public ListNode oddEvenList(ListNode head) {
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = head.next;
+        ListNode temp = head; 
+
+        if (head == null || head.next == null)
+    return head;
+
+         while (even != null && even.next != null) {
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+            odd=odd.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead;
+
+        return temp;
+
+
     }
 }
